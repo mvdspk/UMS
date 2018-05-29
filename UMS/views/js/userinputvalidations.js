a68ce6a92d10b1b2aac51511881aef7f4630b824
mvdspk <123mvds@gmail.com>
2018-05-29 00:11:54 -0700

    signin :extract user name and pwd

--- UMS/views/js/userinputvalidations.js
+++ UMS/views/js/userinputvalidations.js
@@ -8,5 +8,5 @@ function isPwdMatch() {
 function signin(username,pwd){
 var usr=document.getElementById(username).value;
 var password=document.getElementById(pwd).value;
-alert("user name is "+usr+" and password is "+pwd);
+alert("user name is "+usr+" and password is "+password);
 }
