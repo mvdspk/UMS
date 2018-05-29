bed82f1e0d51490a6ae144930c2ea37b4ea501cf
mvdspk <123mvds@gmail.com>
2018-05-29 00:07:31 -0700

    signin :extract user name and pwd

---
+++ views/js/userinputvalidations.js
@@ -0,0 +1,12 @@
+function validateEmail() {
+    alert("hello");
+}
+
+function isPwdMatch() {
+
+}
+function signin(username,pwd){
+var usr=document.getElementById(username).value;
+var password=document.getElementById(pwd).value;
+alert("user name is "+usr+" and password is "+pwd);
+}
