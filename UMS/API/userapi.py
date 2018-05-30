a98e530314e29d19fb27efa1bb0f63a2e0cc565e
Charan <cmalyala@prysm.com>
2018-05-30 11:52:16 +0530

    added ajax and new api for registeration

--- UMS/API/userapi.py
+++ UMS/API/userapi.py
@@ -1,10 +1,18 @@
 from flask import Flask
 app = Flask(__name__)
 
+userDetails = {'username': 'something', 'password': 'something', 'email': 'abc@gmail.com'}
+
 
 @app.route('/')
 def index():
     return 'Welcome to UMS'
+
+
+@app.route('/register')
+def login(request):
+    user = {'username': request['username'], 'password': request['pwd'], 'email': request['email']}
+    return {'msg': 'successful'}
 
 
 if __name__ == "__main__":
