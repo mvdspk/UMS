898b1d96845f3314a523aae80a3ef228f402543f
mvdspk <123mvds@gmail.com>
2018-06-05 16:02:13 -0700

    static css and js served

--- UMS/API/userapi.py
+++ UMS/API/userapi.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory,render_template
+from flask import Flask, render_template
 import os
 app = Flask(__name__, static_url_path='/static')
 
@@ -6,6 +6,7 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 def index():
     return render_template('auth/signin.html')
+
 
 '''send_from_directory(os.path.join(os.getcwd(),'/views/pages'), 'signin.html')'''
 if __name__ == "__main__":
