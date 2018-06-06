c22a5b410323232f63ca367f2418c9ef5a686108
Charan <cmalyala@chtsinc.com>
2018-06-06 14:11:21 -0700

    Added CRUD operations

--- UMS/API/userapi.py
+++ UMS/API/userapi.py
@@ -1,19 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 app = Flask(__name__, static_url_path='/static')
 
 userDetails = {'username': 'something', 'password': 'something', 'email': 'abc@gmail.com'}
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('auth/signin.html')
+    if request.method == 'GET':
+        return render_template('auth/signin.html')
+    else:
+        return {'msg': 'User logged in successfully'}
 
 
-@app.route('/register')
-def login(request):
-    user = {'username': request['username'], 'password': request['pwd'], 'email': request['email']}
-    return {'msg': 'successful'}
+@app.route('/register', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('auth/signin.html')
+    else:
+        user = {'username': request['username'], 'password': request['pwd'], 'email': request['email']}
+        return {'msg': 'successfully created the user please login'}
+
+
+@app.route('/updateuser/<username>', method='PUT')
+def updateuser(username):
+    if request.body is None:
+        return {'error': 'username should not be empty'}
+    else:
+        '''Integration with Cosmos DB  work in progress'''
+        user = {'username': request['username'], 'password': request['pwd'], 'email': request['email']}
+        return {'msg': 'updated user'}
+
+
+@app.route('/deleteuser/<username>', method='DELETE')
+def deleteuser(username):
+    if request.body is None:
+        return {'error': 'username should not be empty'}
+    else:
+        '''Integration with Cosmos DB  work in progress'''
+        return {'msg': 'deleted user'}
 
 
 '''send_from_directory(os.path.join(os.getcwd(),'/views/pages'), 'signin.html')'''
